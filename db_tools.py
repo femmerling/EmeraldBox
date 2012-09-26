ed631062706bcdc7afc551f503614e3cd1236250
Fauzan Emmerling <emmerling@icehousecorp.com>
2012-09-26 18:19:16 +0700

    partially done with model generator. need to work on handler

--- db_tools.py
+++ db_tools.py
@@ -44,23 +44,35 @@ def db_downgrade(version=None):
 	print 'Current database version: ' + str(api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO))
 
 def help():
-	print '\nThe general usage pattern of the tool is python db_tools.py [options]\n'
+	print '\nThe general usage pattern of the tool is python db_tools.py [options] [extra parameters]\n'
 	print 'The following are the available options:\n'
-	print '--create or -c 			: Create the database for the first usage. Only use this if you want to create the database for the first time'
-	print '--migrate or -m 		: Create the migration script for migration process'
-	print '--upgrade or -u 		: Upgrade the database to the latest migration version'
-	print '--downgrade or -d 		: Downgrade the databse to the previous migration version.'
-	print '				  To downgrade to a specific version use python db_tools.py -d [version number]'
-	print '--help or -h 			: Display the help file'
+	print '	--create or -c 			: Create the database for the first usage. Only use this if you want to create the database for the first time'
+	print '	--migrate or -m 		: Create the migration script for migration process'
+	print '	--upgrade or -u 		: Upgrade the database to the latest migration version'
+	print '	--downgrade or -d 		: Downgrade the database to the previous migration version.'
+	print '					  To downgrade to a specific version use python db_tools.py -d [version number]'
+	print '	--new or -n 			: Create new data model.'
+	print '					  The pattern is db_tools.py -n [model name] [<field name>:<field type>,<field length (if applicable. otherwise, default will be used)>]'
+	print '	--help or -h 			: Display the help file'
+	print ''
+
+def add_model(model_name, model_components):
+	basedir = os.path.abspath(os.path.dirname(__file__))
+	model_path = os.path.join(basedir, 'app/models.py')
+	#print model_name
+	#print model_components
+	with open(model_path, 'a') as model_file:
+		model_file.write('')
+		model_file.write('#test')
+
+	#	model_file.write('\n# testline')
+	print 'Model test done'
 
 
 def db_version():
 	current_version = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 	print 'The current database version is ' + current_version
 
-
-#sysinput = sys.argv
-#print sys.argv
 sysinput = sys.argv[1].lower()
 if sysinput == '--help' or sysinput == '-h':
 	help()
@@ -78,3 +90,22 @@ elif sysinput == '--downgrade' or sysinput == '-d':
 		db_downgrade(sys.argv[2])
 	else:
 		db_downgrade()
+elif sysinput == '--new' or sysinput == '-n':
+	if len(sys.argv) > 2:
+		model_name = sys.argv[2].title()
+		raw_component = sys.argv[3:]
+		model_components = []
+		for component in raw_component:
+			raw_field = component.split(':')
+			field_name = raw_field[0]
+			detail_components = raw_field[1].split('--')
+			insert_components = {
+														'field_name':field_name,
+														'field_property': detail_components
+													}
+			model_components.append(insert_components)
+		add_model(model_name,model_components)
+	else:
+		print 'Not enough parameters are provided. See db_tools.py -h for info'
+else:
+	print 'Command not found. Please use --help for command options'
