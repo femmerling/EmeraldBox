ad99a77e08faf3982ca14654a6a067e9f971fcd0
Fauzan Emmerling <erich@emfeld.com>
2012-11-11 01:45:44 +0700

    move default port into variable and added server information

--- ignite.py
+++ ignite.py
@@ -5,7 +5,11 @@ from tornado.ioloop import IOLoop
 from tornado import autoreload
 from app import app
 
+# change the next value for the port being used on your deployment
+server_port = 5000
+
+print "EmeraldBox is now running powered by Tornado Web Server on port " + str(server_port) + " ..."
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000)
+http_server.listen(server_port)
 autoreload.start()
 IOLoop.instance().start()
