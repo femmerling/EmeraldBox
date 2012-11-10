f36c412357152cb0bd3171379921532ea63b9ba4
Fauzan Emmerling <erich@emfeld.com>
2012-11-11 01:34:51 +0700

    cleanup files and simplify reloading

--- ignite.py
+++ ignite.py
@@ -2,12 +2,10 @@
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+from tornado import autoreload
 from app import app
 
-import tornado.autoreload
-
-#app.run(debug=True)
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(2901)
-tornado.autoreload.start()
+autoreload.start()
 IOLoop.instance().start()
