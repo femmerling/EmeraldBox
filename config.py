6f98fb48155e2ad3a2cd71ddf293ed902e695b5e
Fauzan Emmerling <erich@emfeld.com>
2012-12-12 23:42:14 +0700

    updated config to include additional packages parameter to be used during setup

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-import os  # << uncomment for SQLAlchemy Migrate Usage
-basedir = os.path.abspath(os.path.dirname(__file__))  # << uncomment for SQLAlchemy Migrate Usage
+import os
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-#CSRF_ENABLED = True
-#SECRET_KEY = 'j;wD=R#2]07l65r+J)9,%)D[f:1,VS.+RQ+5VY.]lP]\wY:K' << use your own key, this is just an example
+ADDITIONAL_PACKAGES = []
+#additional_packages = ['flask-mail','nose']
 
 #SQLALCHEMY_DATABASE_URI = 'mysql://root:password01@127.0.0.1/flasklearn' # << use this for MySQL, adjust accordingly
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db/app.db')  # << use this for SQLite, adjust accordingly
@@ -10,3 +10,5 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db/app.db')  # <
 #SQLALCHEMY_DATABASE_URI = 'oracle://scott:tiger@127.0.0.1:1521/sidname' #<< use this for oracle, adjust accordingly
 
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')  # << if you want to use SQLAlchemy Migrate
+
+# end of file
