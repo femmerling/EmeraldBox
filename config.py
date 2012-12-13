4c1211a691a0fa19e8e98d176ee1b7989128a023
Fauzan Emmerling <emmerling@icehousecorp.com>
2012-12-13 09:50:17 +0700

    fixed minor bug in config

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ ADDITIONAL_PACKAGES = []
 #additional_packages = ['flask-mail','nose']
 
 #SQLALCHEMY_DATABASE_URI = 'mysql://root:password01@127.0.0.1/flasklearn' # << use this for MySQL, adjust accordingly
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db/app.db')  # << use this for SQLite, adjust accordingly
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'db/app.db')  # << use this for SQLite, adjust accordingly
 #SQLALCHEMY_DATABASE_URI = 'postgresql://scott:tiger@localhost/mydatabase' #<< use this for postgresql, adjust accordingly
 #SQLALCHEMY_DATABASE_URI = 'oracle://scott:tiger@127.0.0.1:1521/sidname' #<< use this for oracle, adjust accordingly
 
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')  # << if you want to use SQLAlchemy Migrate
+SQLALCHEMY_MIGRATE_REPO = os.path.join(BASEDIR, 'db_repository')  # << if you want to use SQLAlchemy Migrate
 
 # end of file
