615876927c2e2b65d091d2b5ed791e97f0bb2701
Fauzan Emmerling <emmerling@icehousecorp.com>
2013-02-01 05:38:54 +0700

    Moved server port setting to config file. Ignite and test run now imports it. Added more information in config file as well

--- ignite.py
+++ ignite.py
@@ -4,11 +4,9 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado import autoreload
 from app import app
+from config import SERVER_PORT
 
-# change the next value for the port being used on your deployment
-server_port = 5000
-
-print "EmeraldBox is now running powered by Tornado Web Server on port " + str(server_port) + " ..."
+print "EmeraldBox is now running powered by Tornado Web Server on port " + str(SERVER_PORT) + " ..."
 
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(server_port)
--- testrun.py
+++ testrun.py
@@ -1,4 +1,5 @@
 #!box/bin/python
 from app import app
+from config import SERVER_PORT
 
-app.run(debug = True)
+app.run(debug = True,port=SERVER_PORT)
