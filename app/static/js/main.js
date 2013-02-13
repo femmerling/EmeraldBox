43c838a78f326c161d739659862245197df9740e
Fauzan Erich Emmerling <erich@emfeld.com>
2013-02-13 22:34:48 +0700

    Changed gitignore and added a bunch of stuffs previously ignored

---
+++ app/static/js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+	var currentUrl = $(location).attr('href');
+	//alert (currentUrl);
+	$('#example-default').dataTable();
+	$('#konreg_list').dataTable();
+	$('#parking_list').dataTable();
+	//$('#rpijm_list').dataTable();
+    //$("table").tablecloth({ theme: "stats" });
+
+
+///Verification Functions
+
+
+});
