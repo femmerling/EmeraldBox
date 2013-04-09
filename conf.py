82a8ef3cbdd5412052e772655c3a9aeb28681818
Fauzan Erich Emmerling <erich@emfeld.com>
2013-04-09 15:30:46 +0700

    Added conf.py for RTD

---
+++ conf.py
@@ -0,0 +1,21 @@
+import os
+# Do not change these values!
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_MIGRATE_REPO = os.path.join(BASEDIR, 'db_repository')
+
+# Add list of python libraries you wish to install on startup in this list
+# Example:
+# additional_packages = ['flask-mail','nose']
+ADDITIONAL_PACKAGES = []
+
+# Select the database connectivity that you wish to use.
+# THe current value defaults to sqlite
+#SQLALCHEMY_DATABASE_URI = 'mysql://root:password01@127.0.0.1/flasklearn' # << use this for MySQL, adjust accordingly
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'db/app.db')  # << use this for SQLite, adjust accordingly
+#SQLALCHEMY_DATABASE_URI = 'postgresql://scott:tiger@localhost/mydatabase' #<< use this for postgresql, adjust accordingly
+#SQLALCHEMY_DATABASE_URI = 'oracle://scott:tiger@127.0.0.1:1521/sidname' #<< use this for oracle, adjust accordingly
+
+# This is the default server port settings that will be used by the system
+SERVER_PORT = 5000
+
+# end of file
