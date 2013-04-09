6f15b236d4bf3bfcd296c436f1f989c1090a2a39
Fauzan Erich Emmerling <erich@emfeld.com>
2013-04-09 15:36:22 +0700

    Adjusted conf.py

--- conf.py
+++ conf.py
@@ -1,21 +1,24 @@
-import os
-# Do not change these values!
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_MIGRATE_REPO = os.path.join(BASEDIR, 'db_repository')
+# -*- coding: utf-8 -*-
+#
+# -- General configuration -----------------------------------------------------
 
-# Add list of python libraries you wish to install on startup in this list
-# Example:
-# additional_packages = ['flask-mail','nose']
-ADDITIONAL_PACKAGES = []
+source_suffix = '.rst'
+master_doc = 'index'
 
-# Select the database connectivity that you wish to use.
-# THe current value defaults to sqlite
-#SQLALCHEMY_DATABASE_URI = 'mysql://root:password01@127.0.0.1/flasklearn' # << use this for MySQL, adjust accordingly
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASEDIR, 'db/app.db')  # << use this for SQLite, adjust accordingly
-#SQLALCHEMY_DATABASE_URI = 'postgresql://scott:tiger@localhost/mydatabase' #<< use this for postgresql, adjust accordingly
-#SQLALCHEMY_DATABASE_URI = 'oracle://scott:tiger@127.0.0.1:1521/sidname' #<< use this for oracle, adjust accordingly
+project = u'EmeraldBox'
+copyright = u'2013, Fauzan Erich Emmerling'
 
-# This is the default server port settings that will be used by the system
-SERVER_PORT = 5000
+version = '0.1.1'
 
-# end of file
+# -- Options for HTML output ---------------------------------------------------
+
+extensions = ['sphinxjp.themecore']
+html_theme = 'dotted'
+
+# -- HTML theme options for `dotted` style -------------------------------------
+
+html_theme_options = {
+    'slidetoc': True,
+    'enablesidebar': True,
+    'rightsidebar': True,
+}
